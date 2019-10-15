72ba03061c8897b4a11b91e45ab3802c0e3434c9
vivien <1456826641@qq.com>
2019-10-15 12:39:26 +0800

    update wheat field

--- index.js
+++ index.js
@@ -4,43 +4,64 @@ $(function() {
     return num;
   }
 
+  // 麦子
   for (let index = 0; index < 200; index ++) {
     let wheat = document.createElement('div');
+    let time = numberRandom(3, 1);
     wheat.className = 'wheat';
     wheat.style =
       'left:' +
         numberRandom(15.5, -0.5) +
       'vw; bottom: ' +
-        numberRandom(5.5, -0.5) +
+        numberRandom(5.5, -2) +
       'vw; animation: Wheatmove ' +
-        numberRandom(3, 1) +
+        time +
       's linear infinite;';
+
+    let wheatSpike = document.createElement('div');
+    wheatSpike.className = 'wheat-spike';
+    wheatSpike.style =
+      'animation: SpikeMove ' +
+        time +
+      's linear infinite;';
+    wheat.appendChild(wheatSpike);
+
+    // let wheatSpikeTwo = document.createElement('div');
+    // wheatSpikeTwo.className = 'wheat-spike';
+    // wheatSpikeTwo.style =
+    //   'animation: SpikeMove ' +
+    //     time +
+    //   's linear infinite; top: 0.6vw; left: 0.2vw;';
+    // wheat.appendChild(wheatSpikeTwo);
+
     let wheats = document.getElementById('wheats');
     wheats.appendChild(wheat);
   }
 
-  for (let index = 0; index < 20; index ++) {
+  // 头发
+  for (let index = 0; index < 40; index ++) {
     let hairOne = document.createElement('div');
     hairOne.className = 'hair-one';
-    hairOne.style =
-      'left:' +
-        (0.1 * index) +
-      'vw; height:' +
-        (2.6 - 0.1 * index) +
-      'vw; transform: rotate('+ (22+2*index)+'deg)';
-    let hair = document.getElementById('hair');
-    hair.appendChild(hairOne);
-  }
-
-  for (let index = 0; index < 20; index ++) {
-    let hairOne = document.createElement('div');
-    hairOne.className = 'hair-one';
-    hairOne.style =
-      'left:' +
-        (2 + 0.1 * index) +
-      'vw; height:' +
-        (0.5 + 0.1 * index) +
-      'vw; transform: rotate('+ (-50+2*index)+'deg)';
+    if (index < 20) {
+      hairOne.style =
+        'left:' +
+          (0.1 * index) +
+        'vw; height:' +
+          (2.6 - 0.1 * index) +
+        'vw; transform: rotate(' +
+          (22 + 2 * index) +
+        'deg)';
+    } else {
+      hairOne.style =
+        'left:' +
+          (1.9 + 0.05 * index) +
+        'vw; height:' +
+          (0.5 + 0.05 * index) +
+        'vw; transform: rotate(' +
+          (-50 + index) +
+        'deg)';
+    }
+    
     let hair = document.getElementById('hair');
     hair.appendChild(hairOne);
   }
